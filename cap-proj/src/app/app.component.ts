a804764817c4926b11e95d1d176f8165cb2fad9b
Paulo Senna <65934431+Sennafox@users.noreply.github.com>
2022-03-17 21:28:35 -0400

    exemplos de diretivas angular

--- cap-proj/src/app/app.component.ts
+++ cap-proj/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cap-proj';
+
+  //ngIF
+
+  exibirTexto = false;
+
+  acaoNgIf(){
+    this.exibirTexto = !this.exibirTexto;
+  }
+
+  //ngFor
+
+  cursos=[" - HTML"," - CSS"," - javascript"," - Angular"]
+
+  //ngSwitch
+
+  curso_selecionado = " - Angular";
+
+  //ngClass
+
+  ligada:boolean = false;
+
+  //data binding
+
+  nome:string = "Paulo";
+  imagem:string = "../assets/suporte-de-mesa-2.jpeg";
+  valor1:number = 2;
+  valor2:number = 3;
+
+  //event binding
+
+  mensagem(){
+    alert("event biding!")
+  }
+
+  //two way data biding
+
+
 }
